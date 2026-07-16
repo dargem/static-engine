2cbdb35cd5e40e942d5ede77cecb25d206a4bab2
dargem <genericz145@gmail.com>
2026-07-16 15:12:39 +1000

    clang format, clang tidy and start integrating logger

--- engine/include/core/logger.h
+++ engine/include/core/logger.h
@@ -1,10 +1,85 @@
 #pragma once
 
-enum class LogLevel {
-    TRACE = 0,
-    DEBUG = 1,
-    INFO  = 2,
-    WARN  = 3,
-    ERROR = 4,
-    FATAL = 5
+#pragma once
+#include <cassert>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+namespace utils {
+
+enum class LogLevel : uint8_t { TRACE = 0, DEBUG, INFO, WARN, ERROR, FATAL };
+
+// A file logger class, will log everything as or more critical than
+// keptLogLevel
+template <LogLevel keptLogLevel> class Logger {
+public:
+  Logger(std::string_view filename) {
+    const std::filesystem::path path{filename};
+
+    // Check if file exists
+    bool fileExists = std::filesystem::exists(path);
+    std::ios_base::openmode mode = fileExists ? std::ios::app : std::ios::out;
+
+    logFile.open(path, mode);
+
+    if (!logFile.is_open()) {
+      if (fileExists) {
+        throw std::runtime_error(
+            "File exists, but unable to open file for writing error logs");
+      }
+      throw std::runtime_error(
+          "Created a file but unable to open file for writing error logs");
+    }
+  }
+
+  Logger(const Logger &) = delete;
+  Logger &operator=(Logger &) = delete;
+
+  // Clean up holding the file
+  ~Logger() = default;
+
+  // Log a message to the logger's file
+  // This call should be completely optimized away at compile time, if it
+  // doesn't log
+  template <LogLevel thisLogsLevel> void log(std::string_view message) {
+    assert(logFile.is_open() && "File should always be open");
+
+    if constexpr (static_cast<int>(thisLogsLevel) >=
+                  static_cast<int>(keptLogLevel)) {
+      // for C++ 20 this works and onwards as a static assert is only evaluated
+      // if the constexpr expression its in is true. Prior to C++ 20 this would
+      // fail at compile time surprisingly
+      static_assert(levelToString(thisLogsLevel) != "UNKNOWN",
+                    "Unknown is a default value for levelToString should never "
+                    "return this");
+      logFile << levelToString(thisLogsLevel) << ": " << message << std::endl;
+      logFile.flush();
+    }
+  }
+
+private:
+  static consteval auto levelToString(LogLevel level) -> std::string_view {
+    switch (level) {
+    case LogLevel::TRACE:
+      return "TRACE";
+    case LogLevel::DEBUG:
+      return "DEBUG";
+    case LogLevel::INFO:
+      return "INFO";
+    case LogLevel::WARN:
+      return "WARN";
+    case LogLevel::ERROR:
+      return "ERROR";
+    case LogLevel::FATAL:
+      return "FATAL";
+    }
+
+    // Fallback, if adding another log level make sure to map it
+    return "UNKNOWN";
+  }
+  std::ofstream logFile;
 };
+
+} // namespace utils
